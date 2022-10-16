9e00838ea6d9dd127e8991f85be4b6682ae04add
Ahmed Mannai <mannaiahmed7518@gmail.com>
2022-10-16 21:16:16 +0100

    Building The Server class for handling multiple clients

--- src/main/java/javachat/client/SimpleChatClientA.java
+++ src/main/java/javachat/client/SimpleChatClientA.java
@@ -16,14 +16,23 @@ public class SimpleChatClientA {
     // GUI and Register a Listener
     public void go() {
 
-        JFrame frame = new JFrame("Java-Chat Client");
+        // Creating the gui components
+        JFrame frame = new JFrame("Java-Chat");
         JPanel mainPanel = new JPanel() ;
         outgoing = new JTextField(20);
         JButton sendButton = new JButton("Send");
+
+        // Initialize the ActionListener with the inner class SendButtonListener that implements ActionListener
         sendButton.addActionListener(new SendButtonListener());
+
+        // Adding the Components to the JPanel
         mainPanel.add(outgoing);
         mainPanel.add(sendButton);
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+
+        // connection to the server
+        setUpNetworking();
+
         frame.setSize(400, 500);
         frame.setVisible(true);
 
@@ -32,9 +41,12 @@ public class SimpleChatClientA {
     private void setUpNetworking(){
 
         try{
+            // Establish Connection to the Server
             sock = new Socket("localhost", 5000);
+            // Initializing the PrintWriter
             writer = new PrintWriter(sock.getOutputStream());
             System.out.println("network established");
+
         }catch(IOException ex){
             ex.printStackTrace();
         }
@@ -46,7 +58,8 @@ public class SimpleChatClientA {
         @Override
         public void actionPerformed(ActionEvent e) {
             try{
-
+                // when button.isPressed -> send message to the server
+                // the inner class implements ActionListener while able to access the global class attributes
                 writer.println(outgoing.getText());
                 writer.flush();
 
@@ -54,6 +67,7 @@ public class SimpleChatClientA {
                 ex.printStackTrace();
             }
 
+            // Resetting the Text Field to empty
             outgoing.setText("");
             outgoing.requestFocus();
         }
